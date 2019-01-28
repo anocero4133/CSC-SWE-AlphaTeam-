b3e91b3dd2bfb35b91ced0725f66a30e06200041
Thong Tran <thong.tran@clxcommunications.com>
2019-01-28 15:28:22 -0500

    Added the Back-end Implementation

---
+++ Back-End/alphaTutor-java-MainService/src/main/java/src/alphaTutorApplication.java
@@ -0,0 +1,11 @@
+package src;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class alphaTutorApplication {
+    public static  void main (String [] args) {
+        SpringApplication.run(alphaTutorApplication.class, args);
+    }
+}
