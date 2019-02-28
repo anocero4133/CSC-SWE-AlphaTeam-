74a0f2777cdf465bf353a5b338508804d5017522
hoa-pham <hpham24@gsu.edu>
2019-02-28 11:20:49 -0500

    add method in Student Service

--- Back-End/alphaTutor-java-MainService/src/main/java/src/Service/StudentService.java
+++ Back-End/alphaTutor-java-MainService/src/main/java/src/Service/StudentService.java
@@ -1,4 +1,17 @@
 package src.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import src.Model.User;
+import src.Repository.UserRepository;
+
+@Service
 public class StudentService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User addUser(User user) {
+        return userRepository.save(user);
+    }
+
 }
