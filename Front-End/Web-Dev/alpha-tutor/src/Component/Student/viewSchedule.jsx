8666582c286a7a4a1728b8a454d6bf70e0bd5e3f
Thong Tran <thong.tran@clxcommunications.com>
2019-03-02 22:29:48 -0500

    Added the line

--- Front-End/Web-Dev/alpha-tutor/src/Component/Student/viewSchedule.jsx
+++ Front-End/Web-Dev/alpha-tutor/src/Component/Student/viewSchedule.jsx
@@ -16,6 +16,7 @@ export default class ViewSchedule extends Component{
         this.listsTutorAvailable = this.listsTutorAvailable.bind(this);
         this.handleIfSearchingEmpty = this.handleIfSearchingEmpty.bind(this);
     }
+    
     possibleTimes = ["10:00 to 10:30", "10:30 to 11:00","11:00 to 11:30", "11:30 to 12:00", "12:00 to 12:30", "12:30 to 13:00", "13:00 to 13:30", "13:30 to 14:00", "14:00 to 14:30", "14:30 to 15:00", "15:00 to 15:30", "15:30 to 16:00", "16:00 to 16:30", "16:30 to 17:00"] 
     tutorSearchName = []
     async fetchAllTutors () { 
