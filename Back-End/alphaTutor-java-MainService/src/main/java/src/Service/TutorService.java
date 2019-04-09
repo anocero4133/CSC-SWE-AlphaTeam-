d982132da677d25b60cb724ac510f9bcdce49672
hoa-pham <hpham24@gsu.edu>
2019-04-08 20:54:58 -0400

    add singleton to Student

--- Back-End/alphaTutor-java-MainService/src/main/java/src/Service/TutorService.java
+++ Back-End/alphaTutor-java-MainService/src/main/java/src/Service/TutorService.java
@@ -4,4 +4,12 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class TutorService {
+    private static TutorService instance;
+    private TutorService() {}
+    public static TutorService getInstance() {
+        if (instance == null) {
+            instance = new TutorService();
+        }
+        return instance;
+    }
 }
